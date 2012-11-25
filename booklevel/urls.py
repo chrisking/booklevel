e0326ba95a6c010c0a956804646dc70355d07d5a
Chris King <chris.king@netapp.com>
2012-11-24 19:38:55 -0500

    prepping to launch on heroku

--- booklevel/urls.py
+++ booklevel/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,3 +17,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
 )
+
+#Add Static Data
+urlpatterns += staticfiles_urlpatterns()
